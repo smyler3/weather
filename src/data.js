5b63b5f36e3dae13f0b232e45189f06d5597bc57
smyler3 <mylertalym01@gmail.com>
2024-01-23 16:53:14 +1100

    Add some error handling logic

--- src/data.js
+++ src/data.js
@@ -1,9 +1,20 @@
 async function getLocationWeatherData(location) {
-  const data = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`,
-  );
-  console.log(data);
-  extractWeatherData(data);
+  try {
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`,
+    );
+
+    console.log(response);
+
+    // Invalid location
+    if (response.status === 400) {
+      return Promise.reject("Location not recognised!");
+    }
+
+    extractWeatherData(response);
+  } catch (error) {
+    return Promise.reject(error);
+  }
 }
 
 async function extractWeatherData(data) {
--- src/index.js
+++ src/index.js
@@ -5,12 +5,27 @@ const submitBtn = document.querySelector("button");
 const searchBar = document.querySelector("#location");
 const searchBarError = document.querySelector("#location + .error-message");
 
+function displaySearchError(error) {
+  searchBarError.textContent = error;
+  searchBar.classList.add("field-error");
+}
+
+function removeSearchError() {
+  searchBar.classList.remove("field-error");
+}
+
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
+  // Data is valid
   if (searchBar.validity.valueMissing) {
-    searchBarError.textContent = "Please enter a value!";
-    searchBar.classList.add("field-error");
+    // Display an error
+    const error = "Please enter a value!";
+    displaySearchError(error);
   } else {
-    getLocationWeatherData(searchBar.value); // Add await?
+    // Add await?
+    removeSearchError();
+    getLocationWeatherData(searchBar.value).catch((error) => {
+      displaySearchError(error);
+    });
   }
 });
